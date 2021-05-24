1bb59a195298426c3e8eb24a722f3dd91c71a8ee
Sakshi Maskara <sakshimaskara@uber.com@Sakshis-MacBook-Pro.local>
2021-05-25 01:40:20 +0530

    Time of execution shown in code

--- Assignment1/crypt.js
+++ Assignment1/crypt.js
@@ -1,4 +1,23 @@
+const {performance} = require('perf_hooks');
 const prompt = require('prompt-sync')();
+var crypto = require('crypto');
+
+var count=1;
+var final= '00000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF';
+const word= prompt('');
+var start = performance.now();
+while(1) {
+    var fin=crypto.createHash('sha256').update(word+count);
+    if(fin.digest('hex')<=final) {
+        break;
+    }
+    count++;
+}
+
+console.log(word+count);
+
+var end = performance.now();
+console.log(`Execution time: ${end - start} ms`);const prompt = require('prompt-sync')();
 var crypto = require('crypto');
 var count=1;
 var final= '00000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF';
